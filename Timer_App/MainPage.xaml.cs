8d8c97230c8b23f4db130696d7d66150d53f9c6d
HeraXiang <lawlietkheel@gmail.com>
2015-03-25 12:22:10 +0800

    change Timer Page

--- Timer_App/MainPage.xaml.cs
+++ Timer_App/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,9 +21,15 @@ namespace Timer_App
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-
     public sealed partial class MainPage : Page
     {
+        int minute=0;
+        int second=0;
+        int rest=0;
+        int minute1, minute2, second1, second2;
+        bool flag = false; //default: not running 
+        
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -49,25 +56,57 @@ namespace Timer_App
         //start
         private void bksclick(object sender, RoutedEventArgs e)
         {
-
+            flag = true; //start running
+            ChangeTime();
         }
 
         //stop
         private void btzclick(object sender, RoutedEventArgs e)
         {
-
+            flag = false;//not running
+            //textblock1.Text = minute1 + minute2+ ":" + second1 + second2;
+            //textblock2.Text = "." + rest;
         }
 
         //clear to zero
         private void bqlclick(object sender, RoutedEventArgs e)
         {
-
+            flag = false;
+            minute = 0;
+            second = 0;
+            rest = 0;
+            textblock1.Text = "00:00";
+            textblock2.Text = "000";
         }
 
         //Set time
         private void bdsclick(object sender, RoutedEventArgs e)
         {
+            //this.NavigationService.Navigate(new Uri("/Time_Page.xaml", UriKind.Relative));
 
+        }
+
+        //change time
+        private void ChangeTime()
+        {
+            while(flag)
+            {
+                //sleep Task.Delay(1);
+
+                minute1 = minute / 10;
+                minute2 = minute % 10;
+                second1 = second / 10;
+                second2 = second % 10;
+                ShowTime();
+            }
+
+        }
+
+        private void ShowTime()
+        {         
+                textblock1.Text = minute1 + minute2 + ":" + second1 + second2;                
+                textblock2.Text = "." + rest;
+                        
         }
     }
 }
